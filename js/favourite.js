056bcef036ecc02ce32d2752246de5bb6d465980
Unknown <ahmed.benhenda@esprit.tn>
2018-11-01 05:28:16 +0100

    favourite list
    get+delete

---
+++ js/favourite.js
@@ -0,0 +1,32 @@
+function loadDate(){
+	var url;
+
+	url = 'http://localhost/android/services.php?action=selectfav&id_user='+1;
+
+	console.log('URL : ' + url);
+	$.getJSON(url, function(jsonData) {
+		console.log(jsonData);
+		for(var k in jsonData){
+			
+			
+
+			 var ul = document.getElementById("action-list");
+			  var li = document.createElement("li");
+			  li.setAttribute("id", jsonData[k]['id_loc']);
+
+			  li.appendChild(document.createTextNode(jsonData[k]['desc_loc']));
+			  li.setAttribute( "onclick", "javascript:showdetail('"+jsonData[k]['id']+"');" );
+			  ul.appendChild(li);
+		}
+		
+	});
+}
+
+loadDate()
+
+function showdetail(id) {
+	console.log(id);
+	sessionStorage.setItem('idloc', id);
+	window.location.assign("detail.html");
+}
+
